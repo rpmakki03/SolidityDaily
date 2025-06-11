cdc1014a349d491f78e3abb45cd34a4a7538718f
Akshat Srivastava <rpmakki03@gmail.com>
2025-06-11 09:16:03 +0530

    Update solidity01.sol

--- solidity01.sol
+++ solidity01.sol
@@ -19,7 +19,7 @@ contract ZombieFactory {
         emit NewZombie(id, _name, _dna);
     }
 
-    fnction _generateRandomDna(string memory _str) private view returns (uint) {
+    function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
